0bbe3598b0dcde144eb9c87c14d398ec75453e57
Fierdolo <federicogentili2000@virgilio.it>
2025-11-15 23:17:35 +0100

    Update urls.py
    feat: add url of dashboard

--- analytics_projects/urls.py
+++ analytics_projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import OverviewAPI
+from . import views
 
 urlpatterns = [
     path('api/overview/', OverviewAPI.as_view(), name='overview-api'), #url of API
+    path('dashboard/', views.dashboard, name='dashboard'), #url of dashboard
+    path('import/', views.import_csv, name='import_csv'),
 ]
-
