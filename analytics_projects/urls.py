9f8cff2bf36985974856cf9a5fde8d24024c6fae
Fierdolo <federicogentili2000@virgilio.it>
2025-11-15 23:47:21 +0100

    Update urls.py
    Feat: add urls of pages of frontend

--- analytics_projects/urls.py
+++ analytics_projects/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('api/overview/', OverviewAPI.as_view(), name='overview-api'), #url of API
     path('dashboard/', views.dashboard, name='dashboard'), #url of dashboard
     path('import/', views.import_csv, name='import_csv'),
+    path('visione_di_insieme_page/', views.visione_di_insieme_page, name='visione_di_insieme_page'), #url of visione d'insieme
+    path('analisi_finanziaria_page/', views.analisi_finanziaria_page, name='analisi_finanziaria_page'), #url of analisi finanziaria
+    path('analisi_settoriale_page/', views.analisi_settoriale_page, name='analisi_settoriale_page'), #url of analisi settoriale
+    path('anomalie_page/', views.anomalie_page, name='anomalie_page'), #url of anomalie page
+    path('beneficiari_page/', views.beneficiari_page, name='beneficiari_page'), #url of beneficiari page
+    path('efficienza_e_performance_page/', views.efficienza_e_performance_page, name='efficienza_e_performance_page'), #url of efficenza e performance page
+    path('territori_e_attori_page/', views.territori_e_attori_page, name='territori_e_attori_page'), #url of territori e attori page
 ]
