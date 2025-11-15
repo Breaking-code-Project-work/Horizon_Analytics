f335c61d760ab7bc40938de9f630f13a743da2b0
Fierdolo <federicogentili2000@virgilio.it>
2025-11-15 23:51:36 +0100

    feat
    feat: add url

--- analytics_projects/views.py
+++ analytics_projects/views.py
@@ -8,6 +8,26 @@ from rest_framework import status
 def dashboard(request):
     return render(request, 'analytics_projects/dashboard.html')
 
+def visione_di_insieme_page(request):
+    return render(request, 'analytics_projects/visione_di_insieme_page.html')
+
+def analisi_finanziaria_page(request):
+    return render(request, 'analytics_projects/analisi_finanziaria_page.html')
+
+def analisi_settoriale_page(request):
+    return render(request, 'analytics_projects/analisi_settoriale_page.html')
+
+def anomalie_page(request):
+    return render(request, 'analytics_projects/anomalie_page.html')
+
+def beneficiari_page(request):
+    return render(request, 'analytics_projects/beneficiari_page.html')
+
+def efficienza_e_performance_page(request):
+    return render(request, 'analytics_projects/efficienza_e_performance_page.html')
+
+def territori_e_attori_page(request):
+    return render(request, 'analytics_projects/territori_e_attori_page.html')
 
 def import_csv(request):
     if request.method == 'POST':
