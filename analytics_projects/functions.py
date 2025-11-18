4fe9a490b481660e74d86ed929cb6496bc4dd85b
ZippoLol <scipollosas@gmail.com>
2025-11-18 19:14:11 +0100

    feat: added functions assigned to me

--- analytics_projects/functions.py
+++ analytics_projects/functions.py
@@ -20,7 +20,49 @@ from django.db.models import Sum
 #Functions that give top 10 projects for financing
 
 #Function that give number of big projects
+def countBigProjects(filters):
+    big_project_threshold = 50_000_000
+
+    projects = Project.objects.all()
+
+    region = filters.get("region")
+    if region and region != "nessun filtro":
+        projects = projects.filter(locations__region=region).distinct()
+
+    macroarea = filters.get("macroarea")
+    if macroarea and macroarea != "nessun filtro":
+        projects = projects.filter(locations__macroarea=macroarea).distinct()
+
+    projects = projects.filter(
+        funding__total_funds_gross__gt=big_project_threshold
+    ).distinct()
+
+    return projects.count()
 
 #Function that give top 3 sectors
+
+#Function that returns the finances given to a specific macroarea
+def fundingByMacroarea(filters):
+    fundings = Funding.objects.all()
+
+    region = filters.get("region")
+    if region and region != "nessun filtro":
+        fundings = fundings.filter(project__locations__region=region)
+
+    macroarea = filters.get("macroarea")
+    if macroarea and macroarea != "nessun filtro":
+        fundings = fundings.filter(project__locations__macroarea=macroarea)
+
+    fundings = fundings.distinct()
+
+    result = fundings.values("project__locations__macroarea").annotate(
+        total=Sum("total_funds_gross")
+    )
+
+    return {
+        item["project__locations__macroarea"]: item["total"] or 0
+        for item in result
+    }
+
 
 
