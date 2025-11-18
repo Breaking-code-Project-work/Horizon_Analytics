41661191d34a5908f44bbcc5babbf94086d2e9c9
ZippoLol <scipollosas@gmail.com>
2025-11-18 22:52:49 +0100

    feat: added functions assigned to me

--- analytics_projects/functions.py
+++ analytics_projects/functions.py
@@ -11,9 +11,9 @@ from analytics_projects.models import Project, Location, Funding
 
 #to use sum on queries
 from django.db.models import Sum
-#Function that give number of projects 
+#Function that give number of projects
 
-#Function that give financing of projects 
+#Function that give financing of projects
 
 #Function that give number of not started, ended and in progress projects
 
@@ -22,20 +22,19 @@ from django.db.models import Sum
 #Function that give number of big projects
 def countBigProjects(filters):
     big_project_threshold = 50_000_000
-
     projects = Project.objects.all()
 
     region = filters.get("region")
     if region and region != "nessun filtro":
-        projects = projects.filter(locations__region=region).distinct()
+        # Filtra progetti che hanno almeno una location con quel region_code
+        projects = projects.filter(locations__region_code=region).distinct()
 
     macroarea = filters.get("macroarea")
     if macroarea and macroarea != "nessun filtro":
         projects = projects.filter(locations__macroarea=macroarea).distinct()
 
-    projects = projects.filter(
-        funding__total_funds_gross__gt=big_project_threshold
-    ).distinct()
+    # Progetti con total_funds_gross >= soglia
+    projects = projects.filter(funding__total_funds_gross__gte=big_project_threshold).distinct()
 
     return projects.count()
 
@@ -47,7 +46,7 @@ def fundingByMacroarea(filters):
 
     region = filters.get("region")
     if region and region != "nessun filtro":
-        fundings = fundings.filter(project__locations__region=region)
+        fundings = fundings.filter(project__locations__region_code=region)
 
     macroarea = filters.get("macroarea")
     if macroarea and macroarea != "nessun filtro":
@@ -55,6 +54,7 @@ def fundingByMacroarea(filters):
 
     fundings = fundings.distinct()
 
+    # Somma totale dei fondi per macroarea
     result = fundings.values("project__locations__macroarea").annotate(
         total=Sum("total_funds_gross")
     )
@@ -63,6 +63,3 @@ def fundingByMacroarea(filters):
         item["project__locations__macroarea"]: item["total"] or 0
         for item in result
     }
-
-
-
