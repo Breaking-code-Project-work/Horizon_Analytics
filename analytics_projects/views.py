f4491b9c299b3026fa86c8530726c4152c21109d
Fierdolo <federicogentili2000@virgilio.it>
2025-11-20 00:08:53 +0100

    fix
    fix: views

--- analytics_projects/views.py
+++ analytics_projects/views.py
@@ -6,40 +6,40 @@ from rest_framework import status
 from .serializers import OverviewSerializer
 from .services import *
 
+def dashboard(request):
+        return render(request, 'analytics_projects/dashboard.html')
+
+def visione_di_insieme_page(request):
+    return render(request, 'analytics_projects/visione_di_insieme_page.html')
+
+def analisi_finanziaria_page(request):
+    return render(request, 'analytics_projects/analisi_finanziaria_page.html')
+
+def analisi_settoriale_page(request):
+    return render(request, 'analytics_projects/analisi_settoriale_page.html')
+
+def anomalie_page(request):
+    return render(request, 'analytics_projects/anomalie_page.html')
+
+def beneficiari_page(request):
+    return render(request, 'analytics_projects/beneficiari_page.html')
+
+def efficienza_e_performance_page(request):
+    return render(request, 'analytics_projects/efficienza_e_performance_page.html')
+
+def territori_e_attori_page(request):
+    return render(request, 'analytics_projects/territori_e_attori_page.html')
+
+def import_csv(request):
+    if request.method == 'POST':
+        return HttpResponse("CSV importato con successo!")
+    return render(request, 'analytics_projects/import_csv.html')
+
 class OverviewAPI(APIView):
     """
     API that recieve filters from frontend and give as output a Jason called data
     """
-    def dashboard(request):
-        return render(request, 'analytics_projects/dashboard.html')
-
-    def visione_di_insieme_page(request):
-        return render(request, 'analytics_projects/visione_di_insieme_page.html')
-
-    def analisi_finanziaria_page(request):
-        return render(request, 'analytics_projects/analisi_finanziaria_page.html')
-
-    def analisi_settoriale_page(request):
-        return render(request, 'analytics_projects/analisi_settoriale_page.html')
-
-    def anomalie_page(request):
-        return render(request, 'analytics_projects/anomalie_page.html')
-
-    def beneficiari_page(request):
-        return render(request, 'analytics_projects/beneficiari_page.html')
-
-    def efficienza_e_performance_page(request):
-        return render(request, 'analytics_projects/efficienza_e_performance_page.html')
-
-    def territori_e_attori_page(request):
-        return render(request, 'analytics_projects/territori_e_attori_page.html')
-
-    def import_csv(request):
-        if request.method == 'POST':
-            return HttpResponse("CSV importato con successo!")
-        return render(request, 'analytics_projects/import_csv.html')
-
-
+    
     def get(self, request):
         # Recover filters from query string
         region = request.query_params.get("region")
