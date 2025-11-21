a326b556e462cb6654892c89047da539aec036d9
Fierdolo <federicogentili2000@virgilio.it>
2025-11-22 00:23:09 +0100

    Fix
    fix: add same working modifics of branch dev after testing

--- analytics_projects/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-class SectorSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    total_financing = serializers.FloatField()
-
-class OverviewSerializer(serializers.Serializer):
-    top_sectors = serializers.DictField(child=SectorSerializer())
--- analytics_projects/urls.py
+++ analytics_projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import OverviewAPI
 
 urlpatterns = [
-    path("api/top-sectors/", OverviewAPI.as_view(), name="top-sectors")
+    
 ]
